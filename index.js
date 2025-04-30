b89848b0a99beaf6bcb7711a5e1e917422fa8c27
Ivaz <ivazreza@gmail.com>
2025-04-30 13:01:06 +0700

    perf: change the logic sequence

--- index.js
+++ index.js
@@ -21,35 +21,29 @@ app.get("/", function (req, res) {
 
 // your first API endpoint... 
 app.get("/api/:date", function (req, res) {
-  const { date } = req.params;
+  const { date: dateParams } = req.params;
+  let dateRes;
 
-  const isDate = /^\d{4}-\d{2}-\d{2}$/.test(date);
-
-  if (isDate) {
-    const dateFormat = new Date(date);
-
-    // get utc to format
-    const dateUtcFormat = dateFormat.toUTCString();
-
-    // get unix time
-    const unixTime = dateFormat.getTime();
-
-    // responses
-    res.json({
-      unix: unixTime,
-      utc: dateUtcFormat,
-    })
+  if (!dateParams) {
+    // No date parameter, use current date
+    dateRes = new Date();
+  } else if (/^\d+$/.test(dateParams)) {
+    // Unix timestamp in milliseconds
+    dateRes = new Date(parseInt(dateParams))
   } else {
-    // get unix to utc
-    const newDate = new Date(Number(date));
-    const dateUtc = newDate.toUTCString();
-  
-    // responses
-    res.json({
-      unix: date,
-      utc: dateUtc,
-    })
+    // Date string
+    dateRes = new Date();
   }
+
+  // Check for invalid date
+  if (dateRes.toString() === 'Invalid Date') {
+    return res.json({ error: 'Invalid Date' });
+  }
+
+  res.json({
+    unix: d.getTime(),
+    utc: d.toUTCString()
+  });
 });
 
 
